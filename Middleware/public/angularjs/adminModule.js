25e9a11484cb5ce728b5ad528fed91728eab380a
neel <neelwithsmile@gmail.com>
2016-04-22 15:33:04 -0700

    Add farmer angularjs

---
+++ Middleware/public/angularjs/adminModule.js
@@ -0,0 +1,29 @@
+var user = angular.module('adminModule',[]);
+user.controller('adminController',['$scope','$http','$sce', function($scope,$http,$sce){
+	
+
+
+	$scope.getfarmers = function(){
+		$http({
+			method : "GET",
+			url : '/farmer/all'
+		}).success(function(res){
+			if (res.status === 200) {
+				console.log("success on getfarmers" + res.data);
+				$scope.farmers = res.data;
+			}
+		});
+	}
+
+	$scope.getproducts = function(){
+		$http({
+			method : "GET",
+			url : '/product/all',
+		}).success(function(res){
+			if (res.status === 200) {
+				console.log("success on getproducts" + res.data);
+				$scope.products = res.data;
+			}
+		});
+	}
+}]);
