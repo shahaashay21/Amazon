ba1fba00b28130189a1dcc583887021ae5861260
neel <neelwithsmile@gmail.com>
2016-04-26 16:54:51 -0700

    Add edit and delete function for farmers

--- Middleware/public/angularjs/adminModule.js
+++ Middleware/public/angularjs/adminModule.js
@@ -1,4 +1,4 @@
-var user = angular.module('adminModule',[]);
+var user = angular.module('adminModule',['xeditable']);
 user.controller('adminController',['$scope','$http','$sce', function($scope,$http,$sce){
 	
 	$scope.getfarmers = function(){
@@ -24,4 +24,53 @@ user.controller('adminController',['$scope','$http','$sce', function($scope,$htt
 			}
 		});
 	}
+
+
+	$scope.saveFarmer = function(data, id) {
+    //$scope.user not updated yet
+	    angular.extend(data, {id: id});
+	    console.log("saveFarmer data::");
+	    console.log(data);
+	    $http({
+			method : "POST",
+			url : '/farmer/edit',
+			data: {
+				f_id : id,
+				fname: data.fname,
+				lname: data.lname,
+				email: data.email,
+				address: data.address
+			}
+		}).success(function(res){
+			if (res.status === 200) {
+				console.log("success on save farmer" + res.data);
+				return;
+				//$scope.farmers = res.data;
+				//return $http.post('/saveUser', data);
+			}
+		});
+  	};
+
+	// remove farmer
+	$scope.removeFarmer = function(id) {
+		//$scope.farmers.splice(index, 1);
+		console.log("removeFarmer ::");
+		console.log(id);
+		$http({
+			method : "DELETE",
+			url : '/farmer/delete',
+			params: {
+				f_id : id
+			}
+		}).success(function(res){
+			if (res.status === 200) {
+				console.log("success on remove farmer" + res.data);
+				return;
+				//$scope.farmers = res.data;
+			}
+		});
+	};
+
+
+	//editableOptions.theme = 'bs3';
 }]);
