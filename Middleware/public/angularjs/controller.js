f56a77a24ac3f4bdc0bc6094ca6df2649a818a85
vivekmehta92 <vivekmehta92@gmail.com>
2016-04-27 20:56:02 -0700

    Product images

--- Middleware/public/angularjs/controller.js
+++ Middleware/public/angularjs/controller.js
@@ -26,7 +26,7 @@
         	checksession();
         	
         	
-            $scope.change_image = function () {  
+            $scope.change_image1 = function () {  
             	console.log("Went on image 1"); 
             	$scope.lrg_image=window.image1;
             };  
@@ -34,7 +34,22 @@
             	console.log("Went on image 2"); 
             	$scope.lrg_image=window.image2;
             }; 
-            
+            $scope.change_image3 = function () {  
+                console.log("Went on image 3"); 
+                $scope.lrg_image=window.image3;
+            };
+            $scope.change_image4 = function () {  
+                console.log("Went on image 4"); 
+                $scope.lrg_image=window.image4;
+            };
+            $scope.change_image5 = function () {  
+                console.log("Went on image 5"); 
+                $scope.lrg_image=window.image5;
+            };
+            $scope.change_image6 = function () {  
+                console.log("Went on image 6"); 
+                $scope.lrg_image=window.image6;
+            };
 
             function create_review()  {
             //$http.post('/create_review')
