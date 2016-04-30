dcf04422db59d2dbcf9581f6037a606949325906
neel <neelwithsmile@gmail.com>
2016-04-29 22:42:46 -0700

    Change order

---
+++ Server/services/model/orderdetail.js
@@ -0,0 +1,29 @@
+var mongoose = require('mongoose');
+
+var orderDetailSchema = mongoose.Schema({
+	o_id: {
+		type: Number,
+		required: true,
+		index: true
+		//ref: 'Order.o_id'
+	},
+	p_id: {
+		type: Number,
+		required: true
+		//ref: 'Product.p_id'
+	},
+	f_id: {
+		type: Number,
+		required: true
+		//ref: 'Farmer.f_id'
+	},
+	qty: Number,
+	price: Number,
+},{
+	collection: 'orderdetails',
+    versionKey: false
+});
+
+var OrderDetail = mongoose.model('OrderDetail', orderDetailSchema);
+
+module.exports = OrderDetail;
